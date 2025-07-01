b0a7a85f31246d3c72a6ed8b92f1d1a26d656eb0
Niazali <niazali031757@gmail.com>
2025-07-01 15:06:05 +0500

    init

--- app/src/main/java/wifi/key/show/master/show/all/wifi/password/Activities/HotSpotShareActivity.java
+++ app/src/main/java/wifi/key/show/master/show/all/wifi/password/Activities/HotSpotShareActivity.java
@@ -40,6 +40,7 @@ public class HotSpotShareActivity extends AppCompatActivity {
     private String TAG = "hellow";
     MyPrefHelper prefHelper;
     private boolean isLocationPermissionEnable() {
+
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 2);
             return false;
